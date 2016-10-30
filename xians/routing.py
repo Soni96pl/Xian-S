294621319eb97c523106e6a11506d521fc748310
Jakub Chronowski <jakub@chronow.ski>
2016-10-30 10:51:36 +0100

    Implement XIANS-16: 'I want to add a favorite with a given name'

--- xians/routing.py
+++ xians/routing.py
@@ -1,8 +1,12 @@
 from xians import api
-from xians.controllers import city, user
+from xians.controllers import city, user, favorite
 
 api.add_resource(city.City, '/city/<int:_id>', endpoint='city_id')
 api.add_resource(city.City, '/city/<string:name>', endpoint='city_name')
 api.add_resource(city.CityDetails, '/city/<int:_id>/details')
 
+
 api.add_resource(user.User, '/users')
+
+
+api.add_resource(favorite.FavoriteAdd, '/favorite/add/<int:city_id>')
