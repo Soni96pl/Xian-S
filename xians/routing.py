2e37e2eb99747ea94a2086c986988a1871716d8a
Jakub Chronowski <jakub@chronow.ski>
2016-10-31 13:59:37 +0100

    Change endpoint for the cities and remove details method since it's now included in fields argument

--- xians/routing.py
+++ xians/routing.py
@@ -1,9 +1,8 @@
 from xians import api
 from xians.controllers import city, user, favorite
 
-api.add_resource(city.City, '/city/<int:_id>', endpoint='city_id')
-api.add_resource(city.City, '/city/<string:name>', endpoint='city_name')
-api.add_resource(city.CityDetails, '/city/<int:_id>/details')
+api.add_resource(city.City, '/cities/<int:_id>', endpoint='city_id')
+api.add_resource(city.City, '/cities/<string:name>', endpoint='city_name')
 
 
 api.add_resource(user.User, '/users')
