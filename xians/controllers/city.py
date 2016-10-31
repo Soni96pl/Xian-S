660fd98887c9c4143ef846c22c4a40487a989ea4
Jakub Chronowski <jakub@chronow.ski>
2016-10-31 14:22:44 +0100

    Don't process cities queried with name to reduce the risk of getting DDoSed by flooding Wikitravel with many requests.

--- xians/controllers/city.py
+++ xians/controllers/city.py
@@ -11,21 +11,24 @@ class City(Resource):
     default_fields = ['_id', 'name', 'coordinates', 'country']
     default_sort = [('population', pymongo.DESCENDING)]
 
-    def process(self, city):
-        if 'story' in city.keys():
-            if (datetime.today() - city['story']['updated']).days > 7:
-                wikitravel.story(city['_id'])
-                city['story']['existing'] = True
-                city['story']['updating'] = True
-            else:
-                city['story']['updating'] = False
-
-        return city
-
     def get(self, _id=None, name=None):
         fields, sort = self.get_query()
 
-        return map(
-            self.process,
-            db.City.search(_id, name, fields=fields, sort=sort)
-        )
+        def process(city):
+            if 'story' in fields:
+                if (datetime.today() - city['story']['updated']).days > 7:
+                    wikitravel.story(city['_id'])
+                    city['story']['existing'] = True
+                    city['story']['updating'] = True
+                else:
+                    city['story']['updating'] = False
+
+            return city
+
+        if _id:
+            return map(
+                self.process,
+                db.City.search(_id=_id, fields=fields, sort=sort)
+            )
+
+        return db.City.search(name=name, fields=fields, sort=sort)
