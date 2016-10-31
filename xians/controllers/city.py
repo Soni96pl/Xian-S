b7405b7d66a0b5efbdeae8d495d80ee731684d84
Jakub Chronowski <jakub@chronow.ski>
2016-10-31 14:01:45 +0100

    Delegate search functionality to the model and take advantage of the new fields arguments

--- xians/controllers/city.py
+++ xians/controllers/city.py
@@ -1,78 +1,31 @@
 from datetime import datetime
-from flask_restful import Resource
 
 import pymongo
 import xiandb as db
 
+from xians.resource import Resource
 from xians.crawlers import wikitravel
 
 
 class City(Resource):
-    def get(self, name=None, _id=None):
-        if _id:
-            params = {
-                '$match': {'_id': _id},
-                'isPerfect': 1
-            }
-        elif name:
-            params = {
-                '$match': {'$or': [
-                    {'name': name}, {'alternate_names': name.lower()}
-                ]},
-                'isPerfect': {'$eq': ['$name', name]}
-            }
-
-        return map(self.process, db.City.aggregate([
-            {'$match': params['$match']},
-            {'$project': {
-                'name': 1,
-                'coordinates': 1,
-                'country': 1,
-                'population': 1,
-                'isPerfect': params['isPerfect']
-            }},
-            {'$sort': {
-                'isPerfect': pymongo.DESCENDING,
-                'population': pymongo.DESCENDING
-            }}
-        ]))
+    default_fields = ['_id', 'name', 'coordinates', 'country']
+    default_sort = [('population', pymongo.DESCENDING)]
 
     def process(self, city):
-        return {
-            'id': city['_id'],
-            'name': city['name'],
-            'coordinates': city['coordinates'],
-            'country': city['country']
-        }
+        if 'story' in city.keys():
+            if (datetime.today() - city['story']['updated']).days > 7:
+                wikitravel.story(city['_id'])
+                city['story']['existing'] = True
+                city['story']['updating'] = True
+            else:
+                city['story']['updating'] = False
 
+        return city
 
-class CityDetails(Resource):
-    def get(self, _id):
-        return map(self.process, db.City.aggregate([
-            {'$match': {'_id': _id}},
-            {'$project': {
-                'name': 1,
-                'coordinates': 1,
-                'country': 1,
-                'population': 1,
-                'story': 1
-            }},
-            {'$limit': 1}
-        ]))
+    def get(self, _id=None, name=None):
+        fields, sort = self.get_query()
 
-    def process(self, city):
-        if (datetime.today() - city['story']['updated']).days > 7:
-            story = {'status': 'updating', 'content': city['story']['content']}
-            wikitravel.story(city['_id'])
-        elif city['story']['existing']:
-            story = {'status': 'existing', 'content': city['story']['content']}
-        else:
-            story = {'status': 'nonexisting'}
-
-        return {
-            'id': city['_id'],
-            'name': city['name'],
-            'coordinates': city['coordinates'],
-            'country': city['country'],
-            'story': story
-        }
+        return map(
+            self.process,
+            db.City.search(_id, name, fields=fields, sort=sort)
+        )
