0ae903bbcff567cb9b9fa980ff1779f964c8c57a
Jakub Chronowski <jakub@chronow.ski>
2016-10-31 13:49:03 +0100

    Use bson's JSON parser by default to allow for better compatibility.

--- xians/__init__.py
+++ xians/__init__.py
@@ -1,11 +1,12 @@
 import os
 
 import yaml
+from bson import json_util
 
-from flask import Flask
+from flask import Flask, make_response
 from flask_log import Logging
 from flask_restful import Api
-from flask_jwt import JWT, jwt_required, current_identity
+from flask_jwt import JWT
 
 import xiandb as db
 
@@ -24,6 +25,13 @@ app.config.from_pyfile('app.cfg')
 api = Api(app)
 flask_log = Logging(app)
 app.cfg = cfg
+
+
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    resp = make_response(json_util.dumps(data), code)
+    resp.headers.extend(headers or {})
+    return resp
 
 
 def authenticate(name, password):
